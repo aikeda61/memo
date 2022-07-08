00b113d76a8f7f4e47f7a2b31d346ae9bd67e936
anna ikeda <ikeda@tsart.jp>
2022-07-08 18:49:48 +0900

    module

---
+++ 220708module/export.js
@@ -0,0 +1,6 @@
+let name = 'anna'
+let age = 27
+let house = 'hyogo'
+let from = 'tokyo'
+
+export {name,age,house,from}
---
+++ 220708module/import.js
@@ -0,0 +1,2 @@
+import {name,age,from,house} from './export.js';
+console.log(age)
