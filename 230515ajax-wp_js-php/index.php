385ba88289b246696882070ccb16a7c190e95c04
mtsknt3 <mtsknt3@gmail.com>
2023-05-15 18:23:37 +0900

    save

--- 230515ajax-wp_js-php/index.php
+++ 230515ajax-wp_js-php/index.php
@@ -1,4 +1,22 @@
-<script>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <main>
+
+        <div id="thoughtsContent"></div>
+        <a href="" id="thoughtsBtn">もっと見る</a>
+
+    </main>
+
+    <script>
+        
     $(function(){
 
         // contents　取得処理
@@ -63,3 +81,8 @@
         })
     })
 </script>
+
+</body>
+</html>
+
+
